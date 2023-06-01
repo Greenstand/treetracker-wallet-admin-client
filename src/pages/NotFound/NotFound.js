06909a7e474022efe8c3d25e1dbad235c5c0a1df
andrehanna8 <andrehanna3@gmail.com>
2023-05-31 19:25:55 -0700

    Created a new component called NotFound.js which can be found in src/components/pages and updated ClientRoutes.js which renders this NotFound component when path isnt specified

---
+++ src/pages/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+// 404 Page
+
+import React from 'react';
+
+const NotFound = () => (
+  <div>
+    <h2>404 - Page not found</h2>
+    <p>We&apos;re sorry, the page you were looking for cannot be found!</p>
+  </div>
+);
+
+export default NotFound;
