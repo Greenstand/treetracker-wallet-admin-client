6bb8971812c11faeb4a2e397a997f442cdc17132
andrehanna8 <andrehanna3@gmail.com>
2023-06-06 21:48:08 -0700

    Moved styling for the NotFound component to a new file called NotFoundStyled.js

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,39 +1,5 @@
-// 404 Page
-
 import React from 'react';
-import styled from '@emotion/styled';
-import { Typography, Container } from '@mui/material';
-
-const Logo = '/logo_192x192.png';
-
-const NotFoundContainer = styled(Container)`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh; // This makes it take up the full height of the viewport
-  background-color: #F0F0F0; 
-`;
-
-const LogoContainer = styled.div`
-  width: 100px; 
-  height: 100px; 
-  margin-bottom: 20px;
-  background-image: url(${Logo});
-  background-size: contain;
-  background-repeat: no-repeat;
-  background-position: center;
-`;
-
-const NotFoundHeader = styled(Typography)`
-  font-size: 3rem; 
-  color: #333; 
-`;
-
-const NotFoundText = styled(Typography)`
-  font-size: 1.5rem; 
-  color: #666;
-`;
+import { NotFoundContainer, LogoContainer, NotFoundHeader, NotFoundText } from './NotFoundStyled';
 
 const NotFound = () => (
   <NotFoundContainer>
