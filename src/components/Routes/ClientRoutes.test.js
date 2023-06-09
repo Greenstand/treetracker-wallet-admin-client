1052cb0f8ea8e333c790d5b5f7f515bc78e51c08
phamminhtan2002 <tanphamminh2002@gmail.com>
2023-06-10 01:22:54 +0700

    update client routes test to include testing for send-tokens page

--- src/components/Routes/ClientRoutes.test.js
+++ src/components/Routes/ClientRoutes.test.js
@@ -1,42 +1,53 @@
-import React from "react";
-import { mount } from "enzyme";
-import { MemoryRouter } from "react-router-dom";
-import ClientRoutes from "./ClientRoutes";
-import Wallet from "../../pages/Wallet/Wallet";
-import TransferStatus from "../../pages/TransferStatus/TransferStatus";
-import apiClient from "../../utils/apiClient";
+import { mount } from 'enzyme';
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import SendTokens from '../../pages/SendTokens/SendTokens';
+import TransferStatus from '../../pages/TransferStatus/TransferStatus';
+import Wallet from '../../pages/Wallet/Wallet';
+import apiClient from '../../utils/apiClient';
+import ClientRoutes from './ClientRoutes';
 
-jest.mock("../../utils/apiClient", () => ({
-  get: jest.fn(),
+jest.mock('../../utils/apiClient', () => ({
+	get: jest.fn(),
 }));
 
-describe("ClientRoutes component", () => {
-  it("should render Wallet component when on '/' route", () => {
-    apiClient.get.mockResolvedValueOnce({
-      data: {
-        id: "9d6c674f-ae62-4fab-8d14-ae5de9f14ab8",
-        logo_url: "https://example.com/logo.png",
-        tokens_in_wallet: 100,
-        wallet: "test wallet",
-      },
-    });
+describe('ClientRoutes component', () => {
+	it("should render Wallet component when on '/' route", () => {
+		apiClient.get.mockResolvedValueOnce({
+			data: {
+				id: '9d6c674f-ae62-4fab-8d14-ae5de9f14ab8',
+				logo_url: 'https://example.com/logo.png',
+				tokens_in_wallet: 100,
+				wallet: 'test wallet',
+			},
+		});
 
-    const wrapper = mount(
-      <MemoryRouter initialEntries={["/"]}>
-        <ClientRoutes />
-      </MemoryRouter>
-    );
+		const wrapper = mount(
+			<MemoryRouter initialEntries={['/']}>
+				<ClientRoutes />
+			</MemoryRouter>,
+		);
 
-    expect(wrapper.find(Wallet)).toHaveLength(1);
-  });
+		expect(wrapper.find(Wallet)).toHaveLength(1);
+	});
 
-  it("should render TransferStatus component when on '/transfer-status' route", () => {
-    const wrapper = mount(
-      <MemoryRouter initialEntries={["/transfer-status"]}>
-        <ClientRoutes />
-      </MemoryRouter>
-    );
+	it("should render TransferStatus component when on '/transfer-status' route", () => {
+		const wrapper = mount(
+			<MemoryRouter initialEntries={['/transfer-status']}>
+				<ClientRoutes />
+			</MemoryRouter>,
+		);
 
-    expect(wrapper.find(TransferStatus)).toHaveLength(1);
-  });
+		expect(wrapper.find(TransferStatus)).toHaveLength(1);
+	});
+
+	it("should render SendTokens component when on '/send-tokens' route", () => {
+		const wrapper = mount(
+			<MemoryRouter initialEntries={['/send-tokens']}>
+				<ClientRoutes />
+			</MemoryRouter>,
+		);
+
+		expect(wrapper.find(SendTokens)).toHaveLength(1);
+	});
 });
