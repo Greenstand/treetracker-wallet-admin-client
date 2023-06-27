6dd18816dc061b1596a017c56e7c09a16b1703d3
phamminhtan2002 <tanphamminh2002@gmail.com>
2023-06-28 02:44:56 +0700

    undo menu styled

--- src/components/layout/Menu/MenuStyled.js
+++ src/components/layout/Menu/MenuStyled.js
@@ -1,10 +1,8 @@
-import { Typography } from '@mui/material';
-import MuiAppBar from '@mui/material/AppBar';
 import MuiDrawer from '@mui/material/Drawer';
 import { styled } from '@mui/system';
 
 const drawerWidth = 240;
-const mobileDrawerWidth = 160;
+const mobileDrawerWidth = 140;
 
 const openedMixin = theme => ({
   width: mobileDrawerWidth,
@@ -39,30 +37,6 @@ const DrawerHeaderStyled = styled('div')(({ theme }) => ({
   ...theme.mixins.toolbar,
 }));
 
-const AppBarStyled = styled(MuiAppBar, {
-  shouldForwardProp: prop => prop !== 'open',
-})(({ theme, open }) => ({
-  backgroundColor: 'white',
-  boxShadow: '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: drawerWidth,
-      width: `calc(100% - ${drawerWidth}px)`,
-    },
-    marginLeft: mobileDrawerWidth,
-    width: `calc(100% - ${mobileDrawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
-
 const DrawerStyled = styled(MuiDrawer, {
   shouldForwardProp: prop => prop !== 'open',
 })(({ theme, open }) => ({
@@ -83,11 +57,4 @@ const DrawerStyled = styled(MuiDrawer, {
   }),
 }));
 
-const LogoStyled = styled(Typography)(({ theme }) => ({
-  '@media(max-width: 480px)': {
-    margin: 'auto',
-    paddingRight: `calc(${theme.spacing(7)} + 1px)`,
-  },
-}));
-
-export { AppBarStyled, DrawerHeaderStyled, DrawerStyled, LogoStyled };
+export { DrawerHeaderStyled, DrawerStyled };
