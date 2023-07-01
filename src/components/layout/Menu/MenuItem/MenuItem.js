8f5810fd924c9df4ba360a4bb3444c220235f91b
Pranav Kakaraparti <pkakaraparti@ufl.edu>
2023-07-01 15:44:38 -0500

    feat: add prettier, refactor menu items

--- src/components/layout/Menu/MenuItem/MenuItem.js
+++ src/components/layout/Menu/MenuItem/MenuItem.js
@@ -1,38 +1,24 @@
 import HomeIcon from '@mui/icons-material/Home';
 import ThumbsUpDownIcon from '@mui/icons-material/ThumbsUpDown';
-import ListItemText from '@mui/material/ListItemText';
 import * as React from 'react';
-import {
-  ItemButtonStyled,
-  ItemIconStyled,
-  LinkItemStyled,
-} from './MenuItemStyled';
+import { Compare } from '@mui/icons-material';
+import LinkItem from './LinkItem';
 
 const MenuItem = ({ open }) => {
   return (
     <>
-      <LinkItemStyled to="/">
-        <ItemButtonStyled open={open}>
-          <ItemIconStyled open={open}>
-            <HomeIcon />
-          </ItemIconStyled>
-          <ListItemText
-            primary={'Home'}
-            sx={{ opacity: open ? 1 : 0, marginLeft: open ? '1rem' : 0 }}
-          />
-        </ItemButtonStyled>
-      </LinkItemStyled>
-      <LinkItemStyled to="/send-tokens">
-        <ItemButtonStyled open={open}>
-          <ItemIconStyled open={open}>
-            <ThumbsUpDownIcon />
-          </ItemIconStyled>
-          <ListItemText
-            primary={'Send Tokens'}
-            sx={{ opacity: open ? 1 : 0, marginLeft: open ? '1rem' : 0 }}
-          />
-        </ItemButtonStyled>
-      </LinkItemStyled>
+      <LinkItem itemPath={'/'}
+                itemName={'Home'}
+                itemIcon={<HomeIcon />}
+                open={open} />
+      <LinkItem itemPath={'/send-tokens'}
+                itemName={'Send Tokens'}
+                itemIcon={<ThumbsUpDownIcon />}
+                open={open} />
+      <LinkItem itemPath={'/transfer-status'}
+                itemName={'Transfer Status'}
+                itemIcon={<Compare />}
+                open={open} />
     </>
   );
 };
