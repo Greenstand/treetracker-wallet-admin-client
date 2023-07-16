37b74d6adbe36bb4fa88e8add8ddca03609e1c84
Pranav Kakaraparti <pkakaraparti@ufl.edu>
2023-07-15 20:11:39 -0500

    feat: display available tokens

--- src/pages/SendTokens/SendTokens.js
+++ src/pages/SendTokens/SendTokens.js
@@ -3,8 +3,18 @@ import apiClient from '../../utils/apiClient';
 import { Loader } from '../../components/UI/components/Loader/Loader';
 import { Grid, Paper } from '@mui/material';
 import ErrorMessage from '../../components/UI/components/ErrorMessage/ErrorMessage';
-import InfoCircle from './InfoCircle';
 import TokenInfoBlock from './TokenInfoBlock';
+import { formatWithCommas } from '../../utils/formatting';
+
+
+const mapWallet = (walletData) => {
+  return {
+    id: walletData.id,
+    logoURL: walletData.logo_url,
+    tokensInWallet: walletData.tokens_in_wallet,
+    name: walletData.wallet,
+  };
+};
 
 const SendTokens = () => {
 
@@ -14,28 +24,48 @@ const SendTokens = () => {
 
 
   //when the page is first rendered
-  useEffect(() => {
-    const loadWallets = async () => {
-      try {
-        // TODO: default limit
-        const wallets = await apiClient.get('/wallets?limit=200');
-        setWalletList(wallets.data.wallets);
-      } catch (error) {
+  // useEffect(() => {
+  //   const loadWallets = async () => {
+  //     try {
+  //       // TODO: default limit
+  //       const wallets = await apiClient.get('/wallets?limit=200');
+  //       setWalletList(wallets.data.wallets);
+  //     } catch (error) {
+  //       console.error(error);
+  //       setErrorMessage('An error occured while fetching wallet data.');
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   };
+  //   loadWallets();
+  // }, []);
+
+
+  useEffect(async () => {
+    setIsLoading(true);
+
+    // TODO: get wallet id by decoding the token. We get the token after login, which is not implemented yet.
+    apiClient
+      .get('/wallets/e46cae85-f0e3-40d4-a637-46d2168bfaad')
+      .then((response) => {
+        const wallet = mapWallet(response.data);
+        setWalletList({ wallet });
+      })
+      .catch((error) => {
         console.error(error);
-        setErrorMessage('An error occured while fetching wallet data.');
-      } finally {
+        setErrorMessage('An error occurred while fetching wallet data.');
+      })
+      .finally(() => {
         setIsLoading(false);
-      }
-    };
-    loadWallets();
+      });
   }, []);
 
   if (isLoading) return <Loader />;
 
   const defaultWallet = walletList.length > 0 ? walletList[0] : {
     id: '',
-    logo_url: '',
-    tokens_in_wallet: 0,
+    logoURL: '',
+    tokensInWallet: 0,
     name: '',
   };
 
@@ -48,23 +78,11 @@ const SendTokens = () => {
           onClose={() => setErrorMessage('')}
         />
       )}
-      <Grid container spacing={2}>
-        <Grid item>
-          <Paper elevation={3}>
-            <div>
-              <InfoCircle innerText={'Available tokens'} innerNumber={defaultWallet.tokens_in_wallet} />
-            </div>
-          </Paper>
-        </Grid>
-        <Grid item>
-          <Paper elevation={3} sx={{ border: '2px solid yellow' }}>
 
-            <TokenInfoBlock />
-
-          </Paper>
-        </Grid>
-      </Grid>
-
+      <Paper elevation={3}>
+        <TokenInfoBlock totalTokens={formatWithCommas(defaultWallet.tokensInWallet)} subWalletName={''}
+                        subWalletTokens={''} />
+      </Paper>
 
     </Grid>
   );
