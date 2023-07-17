f3dd741e4bd953991ff657e34e15eaee5c4d01c2
Pranav Kakaraparti <pkakaraparti@ufl.edu>
2023-07-16 19:38:48 -0500

    feat: updated TopMenu.test.js

--- src/components/layout/Menu/TopMenu/TopMenu.test.js
+++ src/components/layout/Menu/TopMenu/TopMenu.test.js
@@ -1,33 +1,36 @@
-import { shallow } from "enzyme";
-import React from "react";
-import LogoutIcon from "@mui/icons-material/Logout";
-import { AppBarStyled, LogoStyled } from "./TopMenuStyled";
-import TopMenu from "./TopMenu";
+import React from 'react';
+import TopMenu from './TopMenu';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material';
+import theme from '../../../UI/theme';
+import { render, screen } from '@testing-library/react';
 
-describe("TopMenu component", () => {
-  let wrapper;
+describe('TopMenu component', () => {
+  const TestWrapper = (props) => {
+    return <ThemeProvider theme={theme}>
+      <Router>
+        {props.children}
+      </Router>
+    </ThemeProvider>;
+  };
 
-  beforeEach(() => {
-    wrapper = shallow(
-      <TopMenu>
-        <div>Test</div>
-      </TopMenu>
-    );
+  it('renders correctly', async () => {
+    render(<TestWrapper>
+      <TopMenu />
+    </TestWrapper>);
+
+    screen.debug();
+
+    //logo has loaded
+    await screen.findByAltText(/Greenstand logo/);
+
+    expect(screen.getAllByRole('img')).toHaveLength(1);
+    expect(screen.getAllByRole('link')).toHaveLength(1);
+
+    //drawer button
+    expect(screen.getAllByRole('button')).toHaveLength(1);
+    expect(screen.getByRole('button'))
+      .toHaveAttribute('aria-label', expect.stringMatching(/open drawer/));
   });
 
-  it("should render TopMenu component", () => {
-    expect(wrapper).toBeTruthy();
-  });
-
-  it("should render AppBarStyled component", () => {
-    expect(wrapper.find(AppBarStyled)).toHaveLength(1);
-  });
-
-  it("should render LogoStyled component", () => {
-    expect(wrapper.find(LogoStyled)).toHaveLength(1);
-  });
-
-  it("should render LogoutIcon component", () => {
-    expect(wrapper.find(LogoutIcon)).toHaveLength(1);
-  });
 });
