9640d45911a4dc398639fc98d8774aa468361c21
Olha Zakharova <olgadorogko@gmail.com>
2023-07-27 22:26:58 -0400

    fix tests; todo will be fixed later

--- src/components/UI/components/ErrorMessage/ErrorMessage.js
+++ src/components/UI/components/ErrorMessage/ErrorMessage.js
@@ -1,9 +1,9 @@
-import { Alert } from "@mui/material";
+import { Alert } from '@mui/material';
 
 const ErrorMessage = ({ message, onClose }) => {
   return (
-    <div style={{ display: "inline-block", minWidth: "35%" }}>
-      <Alert severity="error" onClose={onClose}>
+    <div style={{ display: 'inline-block', minWidth: '35%' }}>
+      <Alert severity="error" onClose={onClose} role="alert">
         {message}
       </Alert>
     </div>
--- src/pages/SendTokens/SendTokens.test.js
+++ src/pages/SendTokens/SendTokens.test.js
@@ -1,13 +1,13 @@
+import '@testing-library/jest-dom';
 import apiClient from '../../utils/apiClient';
 import { render, screen } from '@testing-library/react';
 import SendTokens from './SendTokens';
 
 jest.mock('../../utils/apiClient', () => ({
-  get: jest.fn(),
+  get: jest.fn(() => Promise.resolve({ data: { wallets: [] } })),
 }));
 
 describe('Display available tokens v1', () => {
-
   const mockData = {
     data: {
       wallets: [
@@ -42,7 +42,7 @@ describe('Display available tokens v1', () => {
     expect(screen.getByRole('progressbar')).toBeInTheDocument();
     await screen.findByRole('alert');
     expect(screen.getByRole('alert')).toHaveTextContent(
-      'An error occured while fetching wallet data.',
+      'An error occured while fetching wallet data.'
     );
   });
 });
