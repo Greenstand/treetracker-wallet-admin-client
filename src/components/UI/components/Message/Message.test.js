99befcf89f9a8431edb06804ef3328b969d2ee6b
Olha Zakharova <olgadorogko@gmail.com>
2023-07-28 15:34:15 -0400

    Merge branch 'v1' into 28-send-tokens-page-v1-send-tokens-to-other-wallet

--- src/components/UI/components/Message/Message.test.js
+++ src/components/UI/components/Message/Message.test.js
@@ -1,37 +1,21 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import { Alert } from '@mui/material';
 import Message from './Message';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 describe('Message component', () => {
-  it('renders without crashing', () => {
-    const wrapper = shallow(<Message message="" onClose={() => {}} />);
-    expect(wrapper.exists()).toBe(true);
-  });
+  it('renders correctly', () => {
+    const mockOnClose = jest.fn();
 
-  it('renders the Alert component with the correct severity', () => {
-    const wrapper = shallow(<Message message="" onClose={() => {}} />);
-    const alertComponent = wrapper.find(Alert);
+    render(<Message message={'TEST MESSAGE'} onClose={mockOnClose} />);
 
-    expect(alertComponent).toHaveLength(1);
-    expect(alertComponent.props().severity).toBe('error');
-  });
+    expect(screen.getByRole('alert')).toBeInTheDocument();
+    expect(screen.getByText(/TEST MESSAGE/)).toBeInTheDocument();
 
-  it('displays the provided error message', () => {
-    const message = 'An error occurred.';
-    const wrapper = shallow(<Message message={message} onClose={() => {}} />);
-    const alertComponent = wrapper.find(Alert);
+    //Close button
+    expect(screen.getAllByRole('button')).toHaveLength(1);
+    const button = screen.getByRole('button', { name: 'Close' });
 
-    expect(alertComponent.props().children).toBe(message);
-  });
-
-  it('calls the onClose function when the Alert is closed', () => {
-    const onCloseMock = jest.fn();
-    const wrapper = shallow(<Message message="" onClose={onCloseMock} />);
-    const alertComponent = wrapper.find(Alert);
-
-    alertComponent.props().onClose();
-
-    expect(onCloseMock).toHaveBeenCalledTimes(1);
+    fireEvent.click(button);
+    expect(mockOnClose).toHaveBeenCalledTimes(1);
   });
 });
