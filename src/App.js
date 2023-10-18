0e7b7becde498c83fb51fcc2135b24e597839d81
emmanue <brinkcorp@gmail.com>
2023-10-18 18:20:21 +0000

    fix: Correct codebase for production

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-mport { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import ClientRoutes from './components/Routes/ClientRoutes';
 import { ThemeProvider } from '@mui/material/styles';
