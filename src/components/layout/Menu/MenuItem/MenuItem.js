a2c6eb7fe6dc9c62e392c8460c6f39ae79e710e5
Mlowegene <genemlowe@gmail.com>
2024-03-01 17:05:22 +0300

    feat: Add link item of the trust relationship page

--- src/components/layout/Menu/MenuItem/MenuItem.js
+++ src/components/layout/Menu/MenuItem/MenuItem.js
@@ -2,6 +2,7 @@ import CompareIcon from '@mui/icons-material/Compare';
 import HomeIcon from '@mui/icons-material/Home';
 import ThumbsUpDownIcon from '@mui/icons-material/ThumbsUpDown';
 import AccountBalanceWalletRoundedIcon from '@mui/icons-material/AccountBalanceWalletRounded';
+import HandshakeIcon from '@mui/icons-material/Handshake';
 import * as React from 'react';
 import LinkItem from './LinkItem';
 
@@ -30,6 +31,12 @@ const MenuItem = ({ open }) => {
         itemPath={'/list-wallets'}
         itemName={'My Wallets'}
         itemIcon={<AccountBalanceWalletRoundedIcon />}
+        open={open}
+      />
+      <LinkItem
+        itemPath={'/trust-relationship'}
+        itemName={'Trust Relationship'}
+        itemIcon={<HandshakeIcon />}
         open={open}
       />
     </>
